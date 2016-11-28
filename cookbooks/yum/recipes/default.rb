67cf6d081ec6393215ed232e43bc9d4be29f0f98
Cardenas Isla <rcardenas20@googlemail.com>
2016-11-28 11:07:14 -0500

    adding apache and yum cookbooks

---
+++ cookbooks/yum/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: yum
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+
+file '/home/centos/windows_workstation' do
+	content 'windows_workstation logs'	
+end
